8215c50b690ef0477407c215a1febebf906ffd54
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-24 13:23:02 -0700

    Safe area issue fixed for main cardview

--- Fructus/Fructus/View/FruitCardView.swift
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -30,12 +30,19 @@ struct FruitCardView: View {
                     .shadow(color:Color(red:0,green: 0,blue: 0,opacity: 0.15),radius: 2,x:2,y:2)
                 
                 // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and widly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal,16)
+                    .frame(maxWidth:480)
+                
                 // BUTTON START
             }
         }//: VSTACK
         .frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity,alignment: .center)
         .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
+        .ignoresSafeArea()
     }
 }
 

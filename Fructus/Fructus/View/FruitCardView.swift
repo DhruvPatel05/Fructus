0b5b20545b1595611a69a61e49120556c5fa7577
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-24 12:39:48 -0700

    View Updated as per design.

--- Fructus/Fructus/View/FruitCardView.swift
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -14,18 +14,28 @@ struct FruitCardView: View {
     
 
     var body: some View {
-        VStack(spacing:20) {
-            // FRUIT: IMAGE
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color:Color(red: 0, green: 0, blue: 0,opacity: 0.15),radius: 8,x:6,y:8)
-        
-            // FRUIT: TITLE
-            Text("Blueberry")
-            // FRUIT: HEADLINE
-            // BUTTON START
+        ZStack {
+            VStack(spacing:20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color:Color(red: 0, green: 0, blue: 0,opacity: 0.15),radius: 8,x:6,y:8)
+                
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color:Color(red:0,green: 0,blue: 0,opacity: 0.15),radius: 2,x:2,y:2)
+                
+                // FRUIT: HEADLINE
+                // BUTTON START
+            }
         }//: VSTACK
+        .frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity,alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 

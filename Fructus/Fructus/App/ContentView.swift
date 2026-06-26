10c23afccda1cceda246349a1b174b771d06765c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-26 16:37:23 -0700

    ContentView UI updated.

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitsData
+    // MARK: - BODY
    
     var body: some View {
-            Text("Hello World")
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                 FruitRowView(fruit: item)
+                        .padding(.vertical,4)
+                }
+            }
+            .navigationTitle("Fruits")
+        }// : NAVIGATION
     }
 
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
+            .previewDevice("iPhone 11 Pro")
         
     }
 }

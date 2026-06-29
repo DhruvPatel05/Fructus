a79dc3661059beeceab505235dcfc1f933caef62
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-29 14:18:53 -0700

    FruitHeaderView Added.

---
+++ Fructus/Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Dhruv Patel on 29/06/26.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View
+    {
+        FruitHeaderView()
+    }
+}
+

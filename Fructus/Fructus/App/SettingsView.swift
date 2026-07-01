0673941267e307354c6b0784c4fff6d42ea2cb07
Dhruv Patel <dhruvpatel913@gmail.com>
2026-07-01 14:02:03 -0700

    SettingView UI updated using the Setting Row view

--- Fructus/Fructus/App/SettingsView.swift
+++ Fructus/Fructus/App/SettingsView.swift
@@ -36,6 +36,23 @@ struct SettingsView: View {
                         }
                  // MARK: - SECTION 2
                     
+                    
+                // MARK: - SECTION 3
+                    GroupBox(
+                        label: SettingsLabelView(lblText:"Application",lblImage:"apps.iphone")
+                    ) {
+                        SettingsRowView(name:"Developer",content: "John/Jane")
+                        SettingsRowView(name:"Designer",content: "Robert Petras")
+                        SettingsRowView(name:"Compatibility",content: "iOS 14")
+                        SettingsRowView(name:"Website",linkLabel: "SwiftUI Masterclass",linkDestination: "swiftuimasterclass.com")
+                        SettingsRowView(name:"Twitter",linkLabel: " @RobertPetras",linkDestination: "twitter.com/robertpetras")
+                        SettingsRowView(name:"SwiftUI",content: "2.0")
+                        SettingsRowView(name:"Version",content: "1.1.0")
+
+                        
+                        
+                    }//: BOX
+                    
                 }//:VSTACK
                 .navigationBarTitle(Text("Settings"),displayMode:.large)
                 .navigationBarItems(
